6a2aafe89dca3c441e4bc31860f36fa69366edd7
Rory Poulter <rorypoulter2@gmail.com>
2024-02-10 17:12:47 +0000

    Update main.py
    Updated the GUI with the GitHub colour palette

--- src/main.py
+++ src/main.py
@@ -38,23 +38,28 @@ def setupRepository():
     gitInit(local_path, remote_repo)
 
 
+def setupWindow(window, path, repo, create_main):
+    font = "SFMono"
+    window.title("GitHub Repository Initializer")
+    window.geometry("600x400")
+    window.config(bg="#24292e")
+
+    Label(window, text="GitHub Repository Initializer", font=(font, 20), bg="#24292e", fg="#ffffff").pack(pady=5)
+
+    Label(window, text="Path:", bg="#24292e", fg="#ffffff", font=font).pack(pady=10)
+    Entry(window, textvariable=path, width=75, bg="#2b3137", fg="#ffffff").pack()
+    Label(window, text="Repository (https):", bg="#24292e", fg="#ffffff", font=font).pack(pady=10)
+    Entry(window, textvariable=repo, width=75, bg="#2b3137", fg="#ffffff").pack()
+    Checkbutton(window, variable=create_main, text="Create main.py", bg="#24292e", fg="#ffffff",
+                activebackground="#24292e", activeforeground="#ffffff", selectcolor="#24292e", font=font).pack()
+    Button(window, text="Setup", command=setupRepository, bg="#2dba4e", fg="#ffffff", font=font, activeforeground="#ffffff",
+           activebackground="#2dba4e", borderwidth=0, width=10).pack(pady=25)
+
+
 if __name__ == "__main__":
-
     root = Tk()
-    root.title("Repository setup")
-    root.geometry("600x400")
-    Label(root, text="GitHub Repository Initializer", font=20).pack(pady=5)
-
     path = StringVar()
     repo = StringVar()
-    Label(root, text="Path:").pack(pady=10)
-    Entry(root, textvariable=path, width=75).pack()
-    Label(root, text="Repo:").pack(pady=10)
-    Entry(root, textvariable=repo, width=75).pack()
-
     create_main = BooleanVar(value=False)
-    Checkbutton(root, variable=create_main, text="Create main.py")
-
-    Button(root, text="Setup", command=setupRepository).pack(pady=25)
-
+    setupWindow(root, path, repo, create_main)
     root.mainloop()
