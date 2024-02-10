a727057bc452634182c6176ce5021cece461a4d6
Rory Poulter <rorypoulter2@gmail.com>
2024-02-10 22:01:48 +0000

    Update GUI.py

--- test/GUI.py
+++ test/GUI.py
@@ -6,20 +6,21 @@ def success():
     messagebox.showinfo("Success", "Repository successfully created!")
 
 
-font = "SFMono"
-
-
 def setupWindow(window, path, repo, create_main):
+    font = "SFMono"
     window.title("GitHub Repository Initializer")
     window.geometry("600x400")
     window.config(bg="#24292e")
+    window.resizable(False, False)
 
-    Label(window, text="GitHub Repository Initializer", font=(font, 20), bg="#24292e", fg="#ffffff").pack(pady=5)
+    Label(window, text="GitHub Repository Initializer", font=(font, 20, "bold"), bg="#24292e", fg="#ffffff").pack(pady=5)
 
-    Label(window, text="Path:", bg="#24292e", fg="#ffffff", font=font).pack(pady=10)
-    Entry(window, textvariable=path, width=75, bg="#2b3137", fg="#ffffff").pack()
-    Label(window, text="Repository (https):", bg="#24292e", fg="#ffffff", font=font).pack(pady=10)
-    Entry(window, textvariable=repo, width=75, bg="#2b3137", fg="#ffffff").pack()
+    Label(window, text="Directory Path:", bg="#24292e", fg="#ffffff", font=font).pack(pady=10)
+    Entry(window, textvariable=path, width=75, bg="#24292e", fg="#ffffff", highlightthickness=1,
+          highlightbackground="#2b3137").pack()
+    Label(window, text="Remote Repository (https):", bg="#24292e", fg="#ffffff", font=font).pack(pady=10)
+    Entry(window, textvariable=repo, width=75, bg="#24292e", fg="#ffffff", highlightthickness=1,
+          highlightbackground="#2b3137").pack()
     Checkbutton(window, variable=create_main, text="Create main.py", bg="#24292e", fg="#ffffff",
                 activebackground="#24292e", activeforeground="#ffffff", selectcolor="#24292e", font=font).pack()
     Button(window, text="Setup", command=success, bg="#2dba4e", fg="#ffffff", font=font, activeforeground="#ffffff",
