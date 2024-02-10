84fbf312311d84f12f60a6f7aedf92d2cb20aa17
Rory Poulter <rorypoulter2@gmail.com>
2024-02-10 17:11:02 +0000

    Update GUI.py

--- test/GUI.py
+++ test/GUI.py
@@ -1,22 +1,35 @@
 from tkinter import *
+from tkinter import messagebox
 
-font = ""
+
+def success():
+    messagebox.showinfo("Success", "Repository successfully created!")
 
 
-root = Tk()
-root.title("GitHub Repository Initializer")
-root.geometry("600x400")
-root.config(bg="#24292e")
+font = "SFMono"
 
-Label(root, text="GitHub Repository Initializer", font=20, bg="#24292e", fg="#ffffff").pack(pady=5)
-path = StringVar()
-repo = StringVar()
-Label(root, text="Path:", bg="#24292e", fg="#ffffff").pack(pady=10)
-Entry(root, textvariable=path, width=75, bg="#2b3137", fg="#ffffff").pack()
-Label(root, text="Repository (https):", bg="#24292e", fg="#ffffff").pack(pady=10)
-Entry(root, textvariable=repo, width=75, bg="#2b3137", fg="#ffffff").pack()
-create_main = BooleanVar(value=False)
-Checkbutton(root, variable=create_main, text="Create main.py", bg="#24292e", fg="#ffffff",
-            activebackground="#24292e", activeforeground="#ffffff", selectcolor="#24292e").pack()
-Button(root, text="Setup", command=..., bg="#2dba4e", fg="#ffffff").pack(pady=25)
-root.mainloop()
+
+def setupWindow(window, path, repo, create_main):
+    window.title("GitHub Repository Initializer")
+    window.geometry("600x400")
+    window.config(bg="#24292e")
+
+    Label(window, text="GitHub Repository Initializer", font=(font, 20), bg="#24292e", fg="#ffffff").pack(pady=5)
+
+    Label(window, text="Path:", bg="#24292e", fg="#ffffff", font=font).pack(pady=10)
+    Entry(window, textvariable=path, width=75, bg="#2b3137", fg="#ffffff").pack()
+    Label(window, text="Repository (https):", bg="#24292e", fg="#ffffff", font=font).pack(pady=10)
+    Entry(window, textvariable=repo, width=75, bg="#2b3137", fg="#ffffff").pack()
+    Checkbutton(window, variable=create_main, text="Create main.py", bg="#24292e", fg="#ffffff",
+                activebackground="#24292e", activeforeground="#ffffff", selectcolor="#24292e", font=font).pack()
+    Button(window, text="Setup", command=success, bg="#2dba4e", fg="#ffffff", font=font, activeforeground="#ffffff",
+           activebackground="#2dba4e", borderwidth=0, width=10).pack(pady=25)
+
+
+if __name__ == "__main__":
+    root = Tk()
+    path = StringVar()
+    repo = StringVar()
+    create_main = BooleanVar(value=False)
+    setupWindow(root, path, repo, create_main)
+    root.mainloop()
